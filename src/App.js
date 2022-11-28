96c651cc159fdc1fd917198790ce1cdf1eca4aa8
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-11-28 10:30:51 +0600

    data load using loader route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About/About';
 import Inventory from './components/Inventory/Inventory';
 import Main from './components/layout/Main';
+import Login from './components/Login/Login';
 import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 
@@ -13,10 +14,19 @@ function App() {
     {
       path: '/', element: <Main></Main>,
       children: [
-        { path: '/', element: <Shop></Shop> },
-        { path: '/orders', element: <Orders></Orders> },
+        {
+          path: '/',
+          loader: () => fetch('products.json'),
+          element: <Shop></Shop>
+        },
+        {
+          path: '/orders',
+          loader: () => fetch('products.json'),
+          element: <Orders></Orders>
+        },
         { path: '/about', element: <About></About> },
         { path: '/inventory', element: <Inventory></Inventory> },
+        { path: '/login', element: <Login></Login> }
       ]
     },
 
