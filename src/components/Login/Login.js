5d74c06733213e7c75bee68bd2f22ebc31f70992
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-12-03 19:20:34 +0600

    create login and signup form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Login.css'
 const Login = () => {
     return (
-        <div>
-            <h1>Welcome to Login Page !!!  </h1>
+        <div className='login-container'>
+            <h4 className='form-tittle'>Login</h4>
+            <form className='form-container'>
+                <div className="from-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="from-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <button className='btn-submit' type='submit' value='login'>Login</button>
+                <p>New to Ema-john? <Link to='/signup'> Create New Account</Link></p>
+            </form>
         </div>
     );
 };
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Signup.css'
 const Signup = () => {
     return (
-        <div>
-            <h3>Sign UP!</h3>
+        <div className='signup-container'>
+            <h4 className='form-tittle'>Sign Up</h4>
+            <form className='form-container'>
+                <div className="from-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="from-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <button className='btn-submit' type='submit' value='login'>Login</button>
+                <p>Already have an account?<Link to='/login'>  Login</Link></p>
+            </form>
         </div>
     );
 };
